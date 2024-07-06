8f83b8b5be51e22672b894738918c0b464f158e7
ParkGiraffe <op5321@soongsil.ac.kr>
2024-07-06 17:02:24 +0900

    Feat: TopSection 구현

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,44 @@
-import Image from "next/image";
+'use client';
+
+import styled from "styled-components";
 import styles from "./page.module.css";
+import StoreIcon from "@mui/icons-material/Store";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>It's 300.1 movement page</h1>
+      <div className={styles.userInfoContainer}>
+        <text>접속자 : 20190320 박요셉</text>
+      </div>
+
+      <div className={styles.topSection}>
+        <h1>현재까지 모인</h1>
+        <h1>ecoin</h1>
+        <h1 className={styles.countText}>15</h1>
+        <Container>
+          <StoreIcon className="store_icon" />
+        </Container>
+      </div>
+
+      <div className={styles.middleSection}>
+        <h3>오늘은 어떤 활동을 하셨나요?</h3>
+      </div>
     </main>
   );
 }
+
+const Container = styled.div`
+  /* width: 20px;
+  height: 20px; */
+
+  .store_icon {
+    width: 45px;
+    height: 45px;
+    
+    path {
+      fill: black;
+    }
+  }
+`;
+
+
