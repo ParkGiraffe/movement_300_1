3e0430044e803f4a2f7b2bc6f2c2c1c5b0d67684
ParkGiraffe <op5321@soongsil.ac.kr>
2024-07-07 01:08:33 +0900

    Feat: Styled-components 적용

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import styled from "styled-components";
 import styles from "./page.module.css";
@@ -11,34 +11,47 @@ export default function Home() {
         <text>접속자 : 20190320 박요셉</text>
       </div>
 
-      <div className={styles.topSection}>
-        <h1>현재까지 모인</h1>
-        <h1>ecoin</h1>
-        <h1 className={styles.countText}>15</h1>
-        <Container>
+      <TopSection>
+        <TopText>현재까지 모인</TopText>
+        <TopText>ecoin</TopText>
+        <TopText className="count_text">15</TopText>
+        <StoreIconContainer>
           <StoreIcon className="store_icon" />
-        </Container>
-      </div>
+        </StoreIconContainer>
+      </TopSection>
 
-      <div className={styles.middleSection}>
-        <h3>오늘은 어떤 활동을 하셨나요?</h3>
-      </div>
+      <MiddleSection>
+        <TitleText>오늘은 어떤 활동을 하셨나요?</TitleText>
+      </MiddleSection>
     </main>
   );
 }
 
-const Container = styled.div`
+const TopSection = styled.div`
+  text-align: center;
+  .count_text {
+    font-size: 120px !important;
+  }
+`;
+
+const TopText = styled.h1`
+  font-size: 30px;
+`;
+
+const MiddleSection = styled.div``;
+
+const TitleText = styled.h3``;
+
+const StoreIconContainer = styled.div`
   /* width: 20px;
-  height: 20px; */
+  height: 20px;  */
 
   .store_icon {
     width: 45px;
     height: 45px;
-    
+
     path {
       fill: black;
     }
   }
 `;
-
-
